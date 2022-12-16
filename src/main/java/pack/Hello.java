966728bc959a383fe930f627e21c885ec83a72c3
Juan José Maneiro <jmaneirogarcia@danielcastelao.org>
2022-12-16 09:40:17 +0100

    Saludos en español

--- src/main/java/pack/Hello.java
+++ src/main/java/pack/Hello.java
@@ -2,6 +2,6 @@ package pack;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+        System.out.println("Hola, grande y bravo mundo.");
     }
 }
